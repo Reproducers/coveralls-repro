b57a921515a9dbb7cc6c25b473d053cd70802914
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-03-08 21:12:21 +0100

    Change to root implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,46 @@
 plugins {
-//    jacoco
+    jacoco
+    id("com.github.nbaztec.coveralls-jacoco") version "1.2.11"
+}
+
+
+tasks.register<JacocoMerge>("jacocoMerge") {
+    group = "verification"
+    subprojects.onEach { subproject -> executionData(subproject.tasks.withType<Test>()) }
+    doFirst { executionData = files(executionData.filter { it.exists() }) }
+}
+
+val sources = subprojects.flatMap { project ->
+    listOf(
+        file("${project.projectDir}/src/commonMain/kotlin"),
+        file("${project.projectDir}/src/jvmMain/kotlin"),
+        file("${project.projectDir}/src/androidMain/kotlin")
+    )
+}
+
+tasks.register<JacocoReport>("jacocoTestReport") {
+    group = "verification"
+
+    dependsOn(subprojects.map { it.tasks.getByName("check") })
+    dependsOn("jacocoMerge")
+
+    val classes = subprojects.map { project ->
+        file("${project.buildDir}/classes/kotlin/jvm/main")
+    }
+
+    classDirectories.setFrom(classes)
+    sourceDirectories.setFrom(files(sources))
+
+    executionData.setFrom(tasks.getByName<JacocoMerge>("jacocoMerge").destinationFile)
+
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = true
+    }
+}
+
+coverallsJacoco {
+    reportPath = "$buildDir/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
+
+    reportSourceSets = sources
 }
--- module-one/build.gradle.kts
+++ module-one/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.4.31"
+    kotlin("multiplatform")
     jacoco
-    id("com.github.nbaztec.coveralls-jacoco") version "1.2.11"
 }
 
 kotlin {
@@ -13,28 +12,4 @@ kotlin {
         }
         named("jvmMain")
     }
-}
-
-tasks.register<JacocoReport>("jacocoTestReport") {
-    group = "verification"
-
-    dependsOn("check")
-
-    val sources = kotlin.sourceSets.flatMap { it.kotlin.srcDirs }
-    val classes = file("${buildDir}/classes/kotlin/jvm/main")
-
-    classDirectories.setFrom(classes)
-    sourceDirectories.setFrom(files(sources))
-    executionData.setFrom("$buildDir/jacoco/jvmTest.exec")
-
-    reports {
-        xml.isEnabled = true
-        html.isEnabled = true
-    }
-}
-
-coverallsJacoco {
-    reportPath = "$buildDir/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
-
-    reportSourceSets = kotlin.sourceSets.flatMap { it.kotlin.srcDirs }
 }
